e4cd889c33a57c9a4e0ac91fbb1e9639481982c7
htmos6 <mehmet.arslan4000@gmail.com>
2023-11-15 11:43:05 +0000

    Server initialized

---
+++ Redis/Server.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+#include <iostream>
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+class Server
+{
+	public: 
+
+
+
+	private: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
